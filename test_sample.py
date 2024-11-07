a7c3467efcf8eace1bb9af138c43cc2dfdaea27b
tranthuc95 <thuc.nguyen@ecotruck.vn>
2024-11-07 18:05:49 +0700

    Update test.py

--- test_sample.py
+++ test_sample.py
@@ -1,12 +1,16 @@
 import unittest
+from unittest.mock import patch
 from app import main
 
 class TestApp(unittest.TestCase):
-    def test_main(self):
-        with self.assertLogs(level='INFO') as log:
-            main(5, 3, 2)  # Truyền tham số vào thay vì nhập qua input
-            self.assertIn("Kết quả của a + b - c là: 6", log.output)
-            self.assertIn("Kết quả của a * b / c là: 7.5", log.output)
+    @patch('builtins.print')  # Mô phỏng hàm print
+    def test_main(self, mock_print):
+        # Thực hiện các phép tính và kiểm tra output
+        main(5, 3, 2)  # Gọi hàm main với các giá trị mẫu
+        
+        # Kiểm tra kết quả đầu ra của print
+        mock_print.assert_any_call("Kết quả của a + b - c là: 6.0")
+        mock_print.assert_any_call("Kết quả của a * b / c là: 7.5")
 
 if __name__ == "__main__":
     unittest.main()
