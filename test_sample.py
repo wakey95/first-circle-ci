a3263d49fad3308d87b4d8a454e870eb43317401
tranthuc95 <thuc.nguyen@ecotruck.vn>
2024-11-07 18:15:23 +0700

    Update tesst.py

--- test_sample.py
+++ test_sample.py
@@ -1,16 +1,11 @@
 import unittest
-from unittest.mock import patch
-from app import main
+from app import calculate
 
 class TestApp(unittest.TestCase):
-    @patch('builtins.print')  # Mô phỏng hàm print
-    def test_main(self, mock_print):
-        # Thực hiện các phép tính và kiểm tra output
-        main(5, 3, 2)  # Gọi hàm main với các giá trị mẫu
-
-        # Kiểm tra kết quả đầu ra của print
-        mock_print.assert_any_call("Kết quả của a + b - c là: 6.0")
-        mock_print.assert_any_call("Kết quả của a * b / c là: 7.5")
+    def test_calculate(self):
+        result1, result2 = calculate(5, 3, 2)
+        self.assertEqual(result1, 6)  # Kiểm tra a + b - c
+        self.assertEqual(result2, 7.5)  # Kiểm tra a * b / c
 
 if __name__ == "__main__":
-    unittest.main(5, 3, 2)
+    unittest.main()
