726427c4bc2eadbe91143c49cecb784465211eab
tranthuc95 <thuc.nguyen@ecotruck.vn>
2024-11-07 18:02:07 +0700

    update

--- test_sample.py
+++ test_sample.py
@@ -5,7 +5,7 @@ class TestApp(unittest.TestCase):
     def test_main(self):
         with self.assertLogs(level='INFO') as log:
             main(5, 3, 2)  # Truyền tham số vào thay vì nhập qua input
-            self.assertIn("Kết quả của a + b - c là: 6.0", log.output)
+            self.assertIn("Kết quả của a + b - c là: 6", log.output)
             self.assertIn("Kết quả của a * b / c là: 7.5", log.output)
 
 if __name__ == "__main__":
