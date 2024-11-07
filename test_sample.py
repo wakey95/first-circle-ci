02cd1e6aa199beb9b5775857a440a9e93486eaee
tranthuc95 <thuc.nguyen@ecotruck.vn>
2024-11-07 18:08:42 +0700

    Update test.py

--- test_sample.py
+++ test_sample.py
@@ -6,8 +6,8 @@ class TestApp(unittest.TestCase):
     @patch('builtins.print')  # Mô phỏng hàm print
     def test_main(self, mock_print):
         # Thực hiện các phép tính và kiểm tra output
-        main(5, 3, 2)  # Gọi hàm main với các giá trị mẫu
-        
+        # main(5, 3, 2)  # Gọi hàm main với các giá trị mẫu
+        main()
         # Kiểm tra kết quả đầu ra của print
         mock_print.assert_any_call("Kết quả của a + b - c là: 6.0")
         mock_print.assert_any_call("Kết quả của a * b / c là: 7.5")
